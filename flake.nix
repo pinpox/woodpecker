ce1494c52366dbafd35625ba131a7e63a712754a
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-01-18 10:43:28 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,175 @@
+{
+  description = "Kitchen Datasheet website";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # apps = forAllSystems
+      #   (system:
+      #     let
+      #       pkgs = nixpkgsFor.${system};
+      #       servescript = pkgs.writeShellScriptBin "serve" ''
+      #         export CMD_DRAW_HEADTABLE="${self.packages.${system}.generate-headtable}/bin/generate-headtable"
+      #         export CMD_DRAW_NUTRIENTS="${self.packages.${system}.generate-nutrients}/bin/generate-nutrients"
+      #         export PATH=$PATH:${pkgs.mdbook-cmdrun}/bin
+      #         echo "Using $CMD_DRAW_HEADTABLE and $CMD_DRAW_NUTRIENTS as generators"
+      #         ${pkgs.mdbook}/bin/mdbook serve --open
+      #       '';
+      #     in
+      #     {
+      #       default = {
+      #         type = "app";
+      #         program = "${servescript}/bin/serve";
+      #       };
+      #     }
+      #   );
+
+      packages = forAllSystems
+        (system:
+          let
+            pkgs = nixpkgsFor.${system};
+
+            vendorSha256 = "sha256-ZavlAFfHshXDdIq7uyNMtdJS4hmVil4ZdipF0pXnIRU=";
+
+            ldflags = [
+              "-s"
+              "-w"
+              "-X github.com/woodpecker-ci/woodpecker/version.Version=${version}"
+            ];
+
+
+            postBuild = ''
+              cd $GOPATH/bin
+              for f in *; do
+                mv -- "$f" "woodpecker-$f"
+              done
+              cd -
+            '';
+          in
+          rec {
+            # bash-example = pkgs.writeShellScriptBin "example-script" ''
+            #   echo test
+            # '';
+
+            # book = pkgs.stdenv.mkDerivation {
+
+            #   name = "book";
+            #   src = ./.;
+            #   buildPhase = null;
+
+            #   CMD_DRAW_HEADTABLE = "${self.packages.${system}.generate-headtable}/bin/generate-headtable";
+            #   CMD_DRAW_NUTRIENTS = "${self.packages.${system}.generate-nutrients}/bin/generate-nutrients";
+
+            #   installPhase = ''
+            #     runHook preInstall
+            #     mdbook build -d $out
+            #     runHook postInstall
+            #   '';
+
+            #   buildInputs = [ pkgs.mdbook pkgs.mdbook-cmdrun ];
+
+            #   meta = with pkgs.lib; {
+            #     homepage = "TODO";
+            #     description = "TODO";
+            #     license = licenses.mit;
+            #     maintainers = [ maintainers.pinpox ];
+            #   };
+
+            # };
+
+
+            # { lib, buildGoModule, callPackage, fetchFromGitHub, pkgs, inputs }:
+
+            woodpecker-frontend = pkgs.stdenv.mkDerivation rec {
+              name = "woodpecker-frontend";
+
+              src = ./web;
+
+
+              nativeBuildInputs = [
+                pkgs.nodejs
+              ];
+
+              buildPhase =
+                let
+                  nodeDependencies = ((import ./node-composition.nix {
+                    inherit pkgs; # nodejs;
+                    inherit (pkgs.stdenv.hostPlatform) system;
+                  }).nodeDependencies.override (old: {
+                    # access to path '/nix/store/...-source' is forbidden in restricted mode
+                    src = src;
+
+                    # dont run the prepare script:
+                    # Error: Cannot find module '/nix/store/...-node-dependencies-jellyfin-web-.../jellyfin-web/scripts/prepare.js
+                    # npm run build:production runs the same command
+                    dontNpmInstall = true;
+                  }));
+                in
+                ''
+                  runHook preBuild
+                  ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+                  export PATH="${nodeDependencies}/bin:$PATH"
+                  npm run build
+                  runHook postBuild
+                '';
+
+              installPhase = ''
+                runHook preInstall
+                mkdir -p $out/share
+                cp -a dist $out/share/jellyfin-web
+                runHook postInstall
+              '';
+            };
+
+            woodpecker-server =
+              pkgs.buildGoModule {
+                name = "woodpecker-server";
+                inherit vendorSha256 ldflags postBuild;
+                src = ./.;
+
+                postPatch = ''
+                  cp -r ${woodpecker-frontend} web/dist
+                '';
+
+                subPackages = "cmd/server";
+                CGO_ENABLED = 1;
+                passthru = { inherit woodpecker-frontend; };
+              };
+
+
+            woodpecker-agent = pkgs.buildGoModule {
+              name = "woodpecker-agent";
+              src = ./.;
+              inherit vendorSha256 ldflags postBuild;
+              subPackages = "cmd/agent";
+              CGO_ENABLED = 0;
+            };
+
+          });
+
+      # defaultPackage = forAllSystems (system: self.packages.${system}.book);
+    };
+}
---
+++ generate-dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p nodePackages.node2nix
+
+# cd "$(dirname $(readlink -f $0))"
+
+node2nix \
+  --node-env node-env.nix \
+  --input ./web/package.json \
+  --output node-packages.nix \
+  --composition node-composition.nix
+  # --nodejs-16 \
+  # --strip-optional-dependencies \
